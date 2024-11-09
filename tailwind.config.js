4216600b639d3279fa29923d91fafd9bfbf53490
thashin04 <thashin2004@gmail.com>
2024-11-09 15:18:10 -0500

    removed some files not needed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkGreen: '#6A7339',
+        lightGreen: '#BBC191',
+        lightTan: '#E0C9AC',
+        offWhite: '#F0ECE1',
+        lightBrown: '#724F54',
+        darkBrown: '#382828' 
+      }
+    },
   },
   plugins: [],
 }
